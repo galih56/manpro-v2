7325ab6f1ae39bbe91afe0e63d87db6f21d45f25
galih56 <galihindra650@gmail.com>
2023-02-01 23:21:20 +0700

    Remove unused code

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,8 +2,9 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { AppProvider } from './providers/app';
 import { AppRoutes } from './routes';
+import './../css/app.css'
 
-class App extends React.Component<{greeting: string}, {count:number}> {
+class App extends React.Component{
    render() {
       return (
          <AppProvider>
@@ -13,5 +14,5 @@ class App extends React.Component<{greeting: string}, {count:number}> {
    }
 }
 ReactDOM.createRoot(document.getElementById('app') as Element).render(     
-   <App greeting='Hellooooooo!!!'/>
+   <App/>
 );
