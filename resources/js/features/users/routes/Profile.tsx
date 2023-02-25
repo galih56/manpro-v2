b3612d0c5f4667f8360f40b662fe77d1027b847e
galih56 <galihindra650@gmail.com>
2023-02-25 16:52:54 +0700

    Rename Profile component

--- resources/js/features/users/routes/Profile.tsx
+++ resources/js/features/users/routes/Profile.tsx
@@ -1,7 +1,7 @@
 import { ContentLayout } from '@/components/Layout';
 import { useAuth } from '@/lib/authentication';
 
-import { UpdateProfile } from '../components/UpdateProfile';
+import { UpdateUser } from '../components/UpdateUser';
 
 type EntryProps = {
   label: string;
@@ -20,14 +20,13 @@ export const Profile = () => {
   if (!auth) return null;
 
   return (
-    <ContentLayout title="Profile">
+    <ContentLayout>
       <div className="bg-white shadow overflow-hidden sm:rounded-lg">
         <div className="px-4 py-5 sm:px-6">
           <div className="flex justify-between">
             <h3 className="text-lg leading-6 font-medium text-gray-900">User Information</h3>
-            <UpdateProfile />
+            <UpdateUser userId={auth.id!}/>
           </div>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details of the auth.</p>
         </div>
         <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
           <dl className="sm:divide-y sm:divide-gray-200">
