a6559e8f83bdc04f9621075bff92f282ac6886f0
galih56 <galihindra650@gmail.com>
2023-02-26 22:44:48 +0700

    Remove unused code

--- resources/js/features/users/types/index.ts
+++ resources/js/features/users/types/index.ts
@@ -1,3 +1,4 @@
+import { Role } from '@/features/roles';
 import { BaseEntity } from '@/types';
 
 export type User = {
@@ -6,9 +7,3 @@ export type User = {
   is_admin: boolean;
   roles : Array<Role>
 } & BaseEntity;
-
-
-export type Role = {
-  name: string;
-} & BaseEntity;
-
