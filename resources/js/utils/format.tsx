d4e56a3648cf3c3270ff9f0beabd349862576371
galih56 <galihindra650@gmail.com>
2023-03-13 13:13:01 +0700

    Delete format.tsx

--- resources/js/utils/format.tsx
+++
@@ -1,4 +0,0 @@
-import { default as dayjs } from 'dayjs';
-
-export const formatDate = (date: number) => dayjs(date).format('D MMMM YYYY');
-export const formatDateTime = (date: number) => dayjs(date).format('D MMMM YYYY  h:mm A');
