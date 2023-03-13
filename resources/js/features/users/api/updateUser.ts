43d61b91e68d8cf3f6765840568c707622c83295
galih56 <galihindra650@gmail.com>
2023-03-13 13:18:05 +0700

    Fix optimistic update for User List

--- resources/js/features/users/api/updateUser.ts
+++ resources/js/features/users/api/updateUser.ts
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 
 import { useAuth } from '@/lib/authentication';
 import { axios } from '@/lib/axios';
-import { MutationConfig } from '@/lib/react-query';
+import { MutationConfig, queryClient } from '@/lib/react-query';
 import { useNotifications } from '@/stores/notifications';
 import { User } from '../types';
 import { Option } from '@/components/Form';
@@ -20,6 +20,9 @@ export const updateUser = ({
   data,
   userId,
 }: UpdateUserDTO): Promise<User> => {
+  if(data.roles){
+    data.roles = data.roles.map((role : any) => role.value);
+  }
   return axios.patch(`/users/${userId}`, data);
 };
 
@@ -32,6 +35,28 @@ export const useUpdateUser = ({ config }: UseUpdateUserOptions = {}) => {
   const { add } = useNotifications();
   const { refetch : refetchUser } = useAuth();
   return useMutation({
+    onMutate: async (updatingUser: any) => {
+      if(updatingUser.roles){
+        updatingUser.roles = updatingUser.roles.map((item : any) => ({
+          id : item.value,
+          name : item.label
+        }));
+      }
+      await queryClient.cancelQueries(['users', updatingUser?.userId]);
+
+      const previousUser = queryClient.getQueryData<User>([
+        'users',
+        updatingUser?.userId,
+      ]);
+      
+      queryClient.setQueryData(['users', updatingUser?.userId], {
+        ...previousUser,
+        ...updatingUser.data,
+        id: updatingUser.userId,
+      });
+
+      return { previousUser };
+    },
     onSuccess: () => {
       add({
         type : 'success',
