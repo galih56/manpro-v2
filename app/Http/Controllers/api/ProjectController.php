d0c837302be277e5eb604a8c4002c522f4ddfdf9
galih56 <galihindra650@gmail.com>
2023-03-13 13:08:22 +0700

    Updated CRUD functions

--- app/Http/Controllers/api/ProjectController.php
+++ app/Http/Controllers/api/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api;
 
 use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProjectController extends Controller
 {
@@ -14,17 +15,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $projects = Project::get();
+        return response()->json($projects);
     }
 
     /**
@@ -35,41 +27,82 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $fields=$request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'progress' => 'numeric',
+            'start_on' => 'nullable|date_format:d-m-Y H:i:s',
+            'due_on' => 'nullable|date_format:d-m-Y H:i:s',
+            'started_at' => 'nullable|date_format:d-m-Y H:i:s',
+            'complated' => 'boolean',
+            'completed_at' => 'nullable|date_format:d-m-Y H:i:s',
+            'completed_by' => 'nullable|numeric',
+        ]);
+
+        $project=Project::create($fields);
+
+        return response()->json([
+            'message' => 'Project created',
+            'data' => $project,
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Project  $project
+     * @param  \App\Models\Project $project
      * @return \Illuminate\Http\Response
      */
-    public function show(Project $project)
+    public function show(int $id)
     {
-        //
-    }
+        $project = Project::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Project $project)
-    {
-        //
+        if(empty($project)){
+            return response([
+                'message' => "Project not found"
+            ],404);
+        }
+        
+        return response()->json(
+            $project
+        );
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Project  $project
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Models\Project $project
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Project $project)
+    public function update(Request $request, $id)
     {
-        //
+        $project = Project::find($id);
+        
+        if(empty($project)){
+            return response([
+                'message' => "Project not found"
+            ],404);
+        }
+        
+        $fields=$request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'progress' => 'numeric',
+            'start_on' => 'nullable|date_format:d-m-Y H:i:s',
+            'due_on' => 'nullable|date_format:d-m-Y H:i:s',
+            'started_at' => 'nullable|date_format:d-m-Y H:i:s',
+            'complated' => 'boolean',
+            'completed_at' => 'nullable|date_format:d-m-Y H:i:s',
+            'completed_by' => 'nullable|numeric',
+        ]);
+        
+        $project->update($fields);
+
+        return response()->json([
+            'message' => 'Project updated',
+            'data' => $project,
+        ]);
     }
 
     /**
@@ -78,8 +111,18 @@ class ProjectController extends Controller
      * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Project $project)
+    public function destroy($id)
     {
-        //
+        $project = Project::find($id);
+        
+        if(empty($project)){
+            return response([
+                'message' => "Project not found"
+            ],404);
+        }
+        $project->delete();
+        return response()->json([
+            'message' => 'Project deleted'
+        ]);
     }
 }
