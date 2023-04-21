333234d94ed69ba962d56080d7d04b406cc9a5df
galih56 <galihindra650@gmail.com>
2023-04-21 21:23:02 +0700

    Text Mask for DatePicker and date format validation

--- resources/js/components/Form/DatePicker.tsx
+++ resources/js/components/Form/DatePicker.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, Fragment, useEffect, useRef, useState } from 'react';
+import React, { ChangeEventHandler, Fragment, ReactNode, useEffect, useRef, useState } from 'react';
 import { format, isAfter, isBefore, isValid, parse } from 'date-fns';
 import { 
   DayPicker, DayPickerProps, useInput,
@@ -6,105 +6,129 @@ import {
 } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import { Popover as UIPopover, Transition } from '@headlessui/react'
-import { CalendarIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { CalendarIcon } from '@heroicons/react/24/outline';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 import clsx from 'clsx';
-import { UseFormRegisterReturn } from 'react-hook-form';
 import { FieldWrapper, FieldWrapperPassThroughProps } from './FieldWrapper';
-
+import { usePopper } from 'react-popper';
+import MaskedInput from 'react-text-mask'
+import { formatDate, validateDateFormat } from '@/utils/datetime';
+import { Badge } from '../elements';
 
 export type DateFieldProps = {
   required?: boolean;
+  value?: string;
   placeholder?: string;
   className?: string;
+  icon? : ReactNode;
+  onChange? : ChangeEventHandler<HTMLInputElement>;
+  onBlur? : ChangeEventHandler<HTMLInputElement>;
+  mask? : string | (string | RegExp)[];
+  guide?: boolean;
+  keepCharPositions?: boolean;
 } & FieldWrapperPassThroughProps ;
 
+export const DateField = ({ value, label, error, placeholder, required, description, className, icon, onChange, onBlur, guide=false, keepCharPositions=false, mask } : DateFieldProps) => {
+  const [ date, setDate ] =useState<string>(); 
 
+  useEffect(()=>{
+    if(value) setDate(value)
+  },[value])
 
-export type DatePickerProps = {
-  required?: boolean;
-  placeholder?: string;
-  className?: string;
-} &  DayPickerProps & FieldWrapperPassThroughProps ;
-
-
-export const DateField = ({ label, error, placeholder, required, description, className } : DateFieldProps) => {
   return (
-    <FieldWrapper label={"Start"} error={error}>
-      <div className="relative">
-          <input type="text" 
-            className={
-              clsx(
-                "w-full appearance-none px-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm",
-                className
-              )} 
-            placeholder="Select date" 
-          />
-          <div className="absolute inset-y-2 right-0 px-2">
-            <XMarkIcon  className="h-6 w-6 opacity-30"/>
+    <FieldWrapper label={label} error={error} description={description}>
+      <MaskedInput
+        mask={mask??[/[0-9]/, /[0-9]/, '-', /[0-9]/, /[0-9]/, '-', /[0-9]/, /[0-9]/, /[0-9]/, /[0-9]/]}
+        placeholder={placeholder}
+        required={required}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={date}
+        guide={guide}
+        showMask={false}
+        keepCharPositions={keepCharPositions}
+        render={(ref : any, props : any) => (
+          <div className="relative">
+            <input type="text" 
+              className={
+                clsx(
+                  "w-full appearance-none px-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm",
+                  className
+                )} 
+              placeholder="Select date" 
+              ref={ref}
+              {...props}
+            />
+            {
+              icon  ? (
+                <div className="absolute inset-y-2 right-0 px-2">
+                  <XMarkIcon  className="h-6 w-6 opacity-30"/>
+                </div>
+              ) : null
+            }
           </div>
-      </div>
+        )}
+      />
     </FieldWrapper>
   )
 }
 
+export type DatePickerProps = {
+  required?: boolean;
+  dateFormat?: string;
+  placeholder?: string;
+  className?: string;
+  iconPosition? : 'right' | 'left'
+} &  DayPickerProps & FieldWrapperPassThroughProps ;
 
-export const DatePicker = ({ mode = 'single',  required=false, className , label = "Select date...", error} : DatePickerProps)=>{
-  const panelRef = useRef<HTMLDivElement>(null);
 
+export const DatePicker = ({ mode = 'single', dateFormat= 'dd-MM-yyyy', required=false, className , label = "Select date...", iconPosition='left',  error} : DatePickerProps)=>{
+  let [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>();
+  let [popperElement, setPopperElement] = useState<HTMLDivElement | null>();
+  let { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement : 'top'
+  });
   const [selected, setSelected] = useState<Date | Date[] | DateRange | undefined>();
-  
-  const [popoverOpen, setPopoverOpen] = useState<boolean>(false);
-
-  const handlePopoverToggle = () => {
-    setPopoverOpen(!popoverOpen);
-    if(popoverOpen && panelRef.current) panelRef.current.scrollIntoView();
-  }
-
-  const handleClickOutside = (event : any) => {
-    if (panelRef.current && !panelRef.current.contains(event.target)) {
-      event.stopPropagation()
-      setPopoverOpen(false);
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  },[]);
 
   const handleSingleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const date = parse(e.target.value, 'y-MM-dd', new Date());
-    if (!isValid(date)) {
+    const date = validateDateFormat(e.target.value,dateFormat);
+    if (date) {
       return setSelected(date);
     }
   };
 
   const handleFromChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const date = parse(e.target.value, 'y-MM-dd', new Date());
-    if (!isValid(date)) {
-      return setSelected({ from: undefined, to: undefined });
-    }
-    if (selected?.to && isAfter(date, selected.to)) {
-      setSelected({ from: selected.to, to: date });
-    } else {
-      setSelected({ from: date, to: selected?.to });
+    const date = validateDateFormat(e.target.value,dateFormat);
+
+    if(date){
+      if (selected?.to && isAfter(date, selected.to)) {
+        setSelected({ from: selected.to, to: date });
+      } else {
+        setSelected({ from: date, to: selected?.to });
+      }
+    }else{
+      setSelected({ from: undefined, to: undefined });
     }
   };
 
   const handleToChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const date = parse(e.target.value, 'y-MM-dd', new Date());
+    const date = validateDateFormat(e.target.value,dateFormat);
 
-    if (!isValid(date)) {
-      return setSelected({ from: selected?.from, to: undefined });
-    }
-    if (selected?.from && isBefore(date, selected.from)) {
-      setSelected({ from: date, to: selected.from });
-    } else {
-      setSelected({ from: selected?.from, to: date });
+    if(date){
+      if (selected?.from && isBefore(date, selected.from)) {
+        setSelected({ from: date, to: selected.from });
+      } else {
+        setSelected({ from: selected?.from, to: date });
+      }
+    }else{
+      setSelected({ from: selected?.from, to: undefined });
     }
   };
+  
+  const handleRemoveDate = ( removedDate : Date)=> {
+    // console.log(selected!.filter((date : Date) => formatDate(date) != formatDate(removedDate)))
+    setSelected(selected!.filter((date : Date) => formatDate(date) != formatDate(removedDate)))
+  }
 
   const handleSelect: SelectSingleEventHandler | SelectRangeEventHandler | SelectMultipleEventHandler = (
     value: DateRange | undefined
@@ -112,109 +136,58 @@ export const DatePicker = ({ mode = 'single',  required=false, className , label
     setSelected(value);
   };
 
-  const Footer = () => {
-      if(mode === "single"){
-        return (
-          <FieldWrapper label={"Start"} error={error}>
-            <div className="relative">
-                <input type="text" 
-                  className={
-                    clsx(
-                      "w-full appearance-none px-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm",
-                      className
-                    )} 
-                  placeholder="Select date" 
-                  onChange={handleSingleChange}
-                />
-                <div className="absolute inset-y-2 right-0 px-2">
-                   <XMarkIcon  className="h-6 w-6 opacity-30"/> 
-                </div>
-            </div>
-          </FieldWrapper>
-        )
-      }
+  const Footer = () => {  
+    if(mode === "single"){
+      return (
+        <FieldWrapper label={label} error={error}>
+          <DateField value={selected ? format(selected as Date,"dd-MM-yyyy") : undefined} onChange={handleSingleChange}/>
+        </FieldWrapper>
+      )
+    }
 
-      if(mode === "range"){
-        return (
-          <>
-            
-            <FieldWrapper label={"Start"} error={error}>
-              <div className="relative">
-                  <input type="text" 
-                    className={
-                      clsx(
-                        "w-full appearance-none px-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm",
-                        className
-                      )} 
-                    placeholder="Select date" 
-                    onChange={handleFromChange}
-                  />
-                  <div className="absolute inset-y-2 right-0 px-2">
-                      { popoverOpen ?  <XMarkIcon  className="h-6 w-6 opacity-30"/> : <CalendarIcon className="h-6 w-6 opacity-30"/>}
-                  </div>
-              </div>
+    if(mode === "range"){
+      return (
+        <div className={'flex'}>
+          <div className='mx-1'>
+            <FieldWrapper label={label} error={error}>
+              <DateField value={selected? format(selected?.from,"dd-MM-yyyy") : undefined} onChange={handleFromChange} />
             </FieldWrapper>
-              
-            <FieldWrapper label={"Start"} error={error}>
-              <div className="relative">
-                  <input type="text" 
-                    className={
-                      clsx(
-                        "w-full appearance-none px-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm",
-                        className
-                      )} 
-                    placeholder="Select date" 
-                    onChange={handleToChange}
-                  />
-                  <div className="absolute inset-y-2 right-0 px-2">
-                      { popoverOpen ?  <XMarkIcon  className="h-6 w-6 opacity-30"/> : <CalendarIcon className="h-6 w-6 opacity-30"/>}
-                  </div>
-              </div>
+          </div>
+          <div className='mx-1'>
+            <FieldWrapper label={label} error={error}>
+              <DateField value={selected? format(selected?.to,"dd-MM-yyyy") : undefined} onChange={handleToChange}/>
             </FieldWrapper>
-          </>
-        )
+          </div>
+        </div>
+      )
     }
     return null;
   }
+  
 
   return (
     <>
       <UIPopover>
-        <div className="inline-flex">
-          <div className="hover:cursor-pointer text-right " onClick={handlePopoverToggle}>
-            { selected ? (
-              <label className={
-                clsx(
-                  "w-full appearance-none px-3 shadow-sm focus:outline-none sm:text-sm",
-                  className
-                )} 
-              >{label}</label>
-            ): (
-
-              <label className={
-                clsx(
-                  "w-full appearance-none px-3 shadow-sm focus:outline-none sm:text-sm",
-                  className
-                )} 
-              >Select date...</label>
-            )}
-          </div>
-        
-          <div className="basis-1/5 px-2 hover:cursor-pointer" onClick={handlePopoverToggle}>
-              { popoverOpen ?  <XMarkIcon  className="h-6 w-6 opacity-30"/> : <CalendarIcon className="h-6 w-6 opacity-30"/>}
-          </div>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-200"
-          enterFrom="opacity-0 translate-y-1"
-          enterTo="opacity-100 translate-y-0"
-          leave="transition ease-in duration-50"
-          leaveFrom="opacity-100 translate-y-0"
-          leaveTo="opacity-0 translate-y-1"
-          show={popoverOpen}
-        >
-            <UIPopover.Panel ref={panelRef} className={"absolute right-6 z-10 mt-4 rounded-xl bg-white drop-shadow-lg"} static  >
+        {({ open }) => (
+          <>
+            <div className={"inline-flex align-middle"}>
+              <UIPopover.Button  ref={setReferenceElement}  
+                  className={clsx(
+                    "px-2 hover:cursor-pointer",
+                    iconPosition == "left"? "flex-row-reverse" : "" 
+                  )}>
+                    { open ?  <XMarkIcon  className="h-6 w-6 opacity-30"/> : <CalendarIcon className="h-6 w-6 opacity-30"/>}
+                </UIPopover.Button>
+                <div className="hover:cursor-pointer text-right align-middle mx-2">
+                  <SelectedDates mode={mode} selected={selected} label={label} onRemove={handleRemoveDate}/>
+                </div>
+            </div>
+            <UIPopover.Panel 
+              ref={setPopperElement}
+              style={styles.popper}
+              {...attributes.popper}
+              className={" z-40 mt-2 mb-6 mx-2 rounded-xl bg-white drop-shadow-lg"} 
+              >
               <DayPicker
                 mode={mode}
                 selected={selected}
@@ -222,10 +195,68 @@ export const DatePicker = ({ mode = 'single',  required=false, className , label
                 footer={<Footer/>}
               />
             </UIPopover.Panel>
-        </Transition>
+          </>
+        )}
       </UIPopover>
     </>
   );
 }
 
+export type SelectedDateProps = {
+  mode: 'default' | 'single' | 'multiple' | 'range';
+  label? : string;
+  onRemove: (date : Date) => void
+  placeholder?: string; 
+  selected?: Date | Date[] | DateRange | undefined;
+};
+
+
+
+const SelectedDates = ({ mode, selected, label, onRemove } : SelectedDateProps) => {
+  if(selected) {
+    if(mode === 'multiple'){
+      return (
+        <div className="flex flex-wrap">
+          {selected.map((date : Date, i : number)=>(
+              <Badge key={`${date.toISOString()}-${i}`} variant='default' className={"inline-flex mt-2"}>
+                {formatDate(date)} 
+                <XMarkIcon className='h-4 h-4 ml-2 hover:fill-red-900 px-0' onClick={()=> onRemove(date)}/>
+              </Badge>
+            )
+          )}
+        </div>
+       ); 
+     }
+     if(mode === 'range'){
+       return (
+           <div className="inline-flex">
+              <label className={
+                clsx(
+                  "w-full focus:outline-none sm:text-sm",
+                )} 
+              >
+                {formatDate(selected.from)} - {formatDate(selected.to)}
+              </label>
+           </div>
+       )
+    }
+    if(mode === "single"){
+      return (
+        <label className={
+          clsx(
+            "w-full focus:outline-none sm:text-sm",
+          )} 
+        >{formatDate(selected as Date)}</label>
+      )
+    }
+  }  
+
+  return (
+    <label className={
+      clsx(
+        "w-full focus:outline-none sm:text-sm",
+      )} 
+    >{label ?? "Select date..."}</label>
+  );
+}
 
--- resources/js/utils/datetime.tsx
+++ resources/js/utils/datetime.tsx
@@ -1,6 +1,35 @@
-import {parse, format, parseISO} from 'date-fns';
+import {parse, format, parseISO, isValid} from 'date-fns';
 
-export const formatDate = (date: string) => format(parseISO(date),'d MMM yyyy');
-export const formatDateTime = (date: string) => format(parseISO(date),'d MMM yyyy hh:mm:ss');
+export const validateDateFormat = (date : string, format: string) => {
+    if(date && format.length == date.length){
+        const value = parse(date, format, new Date());
+        if(isValid(value)){
+            return value
+        }
+    }
+    return null;
+}
+
+export const formatDate = (date: number | Date | string) => {
+    if(date){
+        if(typeof date === "string"){
+            date = parseISO(date);
+        }
+        date = format(date, 'd MMM yyyy');
+        return date
+    }
+    return "";
+};
+
+export const formatDateTime = (date: number | Date | string) => {
+    if(date){
+        if(typeof date === "string"){
+            date = parseISO(date);
+        }
+        date = format(date, 'd MMM yyyy hh:mm:ss');
+        return date
+    }
+    return "";
+};
 
 export const wait = (time : number) => new Promise(resolve => setTimeout(resolve, time));
