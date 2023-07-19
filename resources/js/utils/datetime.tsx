a7ad98ad59d0c9d28ba63f51a1db8731b9706207
galih56 <galihindra650@gmail.com>
2023-07-19 19:50:46 +0700

    Updated datetime timezone formatting

--- resources/js/utils/datetime.tsx
+++ resources/js/utils/datetime.tsx
@@ -1,5 +1,12 @@
-import {parse, format, parseISO, isValid} from 'date-fns';
+import {parse, format, parseISO, isValid } from 'date-fns';
+import utcToZonedTime from "date-fns-tz/utcToZonedTime";
+import zonedTimeToUtc from "date-fns-tz/zonedTimeToUtc";
 
+export function parseWithTimeZone(date : string, format : string, referenceDate : any, timeZone : string) {
+  const zonedDate = utcToZonedTime(referenceDate, timeZone);
+  const parsedDate = parse(date, format, zonedDate);
+  return zonedTimeToUtc(parsedDate, timeZone);
+}
 export const validateDateFormat = (date : string, format: string) => {
     if(date && format.length == date.length){
         const value = parse(date, format, new Date());
@@ -32,4 +39,26 @@ export const formatDateTime = (date: number | Date | string) => {
     return "";
 };
 
+export const convertDates = (data : any, callback  : Function | null) : any=> {
+  if (data instanceof Array) {
+    return data.map((item) => convertDates(item,callback));
+  } else if (data instanceof Object) {
+    for (const key in data) {
+        // const date = parseWithTimeZone(data[key], "yyyy-MM-dd'T'HH:mm:ssXXX", new Date(), "UTC");
+        const date = parseISO(data[key]);
+        
+        if (isValid(date)) {
+            data[key] = date;
+        } else if (typeof data[key] === "object") {
+            data[key] = convertDates(data[key],callback);
+        }
+        console.log(callback)
+        // use callback to do extra data manipulation
+        if(callback){ 
+            data = callback(key, data);
+        }
+    }
+  }
+  return data;
+};
 export const wait = (time : number) => new Promise(resolve => setTimeout(resolve, time));
