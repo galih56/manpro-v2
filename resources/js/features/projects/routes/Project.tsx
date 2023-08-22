27defc4e0fde8a152e28f338d1a3507960db70ad
galih56 <galihindra650@gmail.com>
2023-08-22 08:53:06 +0700

    Fix project routes

--- resources/js/features/projects/routes/Project.tsx
+++ resources/js/features/projects/routes/Project.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { Spinner, MDPreview } from '@/components/Elements';
 import { Head } from '@/components/Head';
@@ -8,12 +8,12 @@ import { formatDate } from '@/utils/datetime';
 
 import { useProject } from '../api/getProject';
 import { UpdateProject } from '../components/UpdateProject';
-import { TabRoutes } from '../components/Tabs/TabRoutes';
-import { ProjectTabs } from '../components/Tabs/Tabs';
+import { ProjectDetail } from '../components/ProjectDetail';
+
+import { TasksProvider, useTasks } from '@/stores/tasks';
 
 export const Project = () => {
   const { projectId } = useParams();
-  
   const projectQuery = useProject({ projectId });
 
   if (projectQuery.isLoading) {
@@ -25,34 +25,29 @@ export const Project = () => {
   }
 
   if (!projectQuery.data) return null;
+  
 
+  const project = projectQuery.data;
+  
   return (
     <>
       <Head title={projectQuery.data.title} />
       <ContentLayout title={projectQuery.data.title}>
-        <span className="text-xs font-bold">{formatDate(projectQuery.data.createdAt)}</span>
-        <div className="mt-6 flex flex-col space-y-16">
-          <div className="flex justify-end">
-            <UpdateProject projectId={projectId!} />
-          </div>
-          <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-            <div className="px-4 py-5 sm:px-6">
-              <div className="mt-1 text-sm text-gray-500">
-              <Routes>
-                <Route element={<ProjectTabs project={projectQuery.data}/>}>
-                  {TabRoutes.map(({ element, path }) => (
-                    <Route key={path} {...{ element, path }} />
-                  ))}
-                  <Route
-                    path="*"
-                    element={<Navigate to={TabRoutes[0].path} replace />}
-                  />
-                </Route>
-              </Routes>
+          <span className="text-xs font-bold">{formatDate(projectQuery.data.createdAt)}</span>
+          <div className="mt-6 flex flex-col space-y-16">
+            <div className="flex justify-end">
+              <UpdateProject projectId={projectId!} />
+            </div>
+            <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+              <div className="px-4 py-5 sm:px-6">
+                <div className="mt-1 text-sm text-gray-500">
+                  <TasksProvider>
+                    <ProjectDetail project={projectQuery.data}/>
+                  </TasksProvider>
+                </div>
               </div>
             </div>
           </div>
-        </div>
       </ContentLayout>
     </>
   );
