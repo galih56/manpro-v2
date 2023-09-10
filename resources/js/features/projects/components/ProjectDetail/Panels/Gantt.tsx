7cc88a11548962f630d12fdba00767e2a2073408
galih56 <galihindra650@gmail.com>
2023-09-10 22:12:58 +0700

    Seperate Update form from drawer

--- resources/js/features/projects/components/ProjectDetail/Panels/Gantt.tsx
+++ resources/js/features/projects/components/ProjectDetail/Panels/Gantt.tsx
@@ -12,14 +12,42 @@ import { Drawer, DrawerProps } from '@/components/Elements/Drawer';
 import { Button } from '@/components/Elements/Button';
 import { FormUpdateTask } from "@/features/tasks/components/FormUpdateTask";
 import { useTask } from "@/features/tasks/api/getTask";
-import { useProjectOptions } from "@/hooks/useProjectOptions";
-import { useTagOptions } from "@/hooks/useTagOptions";
-import { useUserOptions } from "@/hooks/useUserOptions";
-import { UpdateTaskDTO, useUpdateTask } from "@/features/tasks/api/updateTask";
-import { Form, InputField, SelectField, TextAreaField } from "@/components/Form";
+import {  useUpdateTask } from "@/features/tasks/api/updateTask";
 import * as z from 'zod';
 import { DatePicker } from "@/components/Elements/DatePicker";
 import { Spinner } from "@/components/elements";
+
+const retructureTasks = (data : any[]) => {
+    var tasks : Array<any> = [];
+    data.forEach((task : any) => {
+        for (const key in task) {
+            const date = parseISO(task[key]);
+                        
+            if (isValid(date)) {
+                task[key] = date;
+            } 
+            
+
+            if(key == "startOn" ){
+                if(task[key]) task.start = task[key];
+            }
+
+            if(key == "dueOn"){
+                if(task[key]) task.end = task[key];
+            }
+
+
+            if(key == "taskId"){
+                if(task[key]) task.dependencies = [ task[key] ];
+            }
+            if(key == "title" && task[key]){
+                if(task[key]) task.name = task[key];
+            }
+        }
+        tasks.push(task)
+    }); 
+    return tasks;
+}
 
 export const Gantt = withTasks((props) => {
     const { close, open, isOpen } = useDisclosure(false);
@@ -28,25 +56,21 @@ export const Gantt = withTasks((props) => {
 
     const { tasks } = useTasks();
     
-    useEffect(()=>{
-        console.log(tasks)
-    },[isOpen]);
-    
     if(!tasks.length){
         return (
             <NoEntriesFound/>
         )
     }
-
+    
     const onClick = (task : Task) => {
         setClickedTask(task);
         open();
     }
-
+    const timeline = retructureTasks(tasks)
     return (
         <>
             <GanttChart  
-                tasks={tasks} 
+                tasks={timeline} 
                 onClick={onClick}
 
             />
@@ -55,18 +79,6 @@ export const Gantt = withTasks((props) => {
     )
 })
 
-
-const schema = z.object({
-    projectId: z.string(),
-    title: z.string().min(1,'Required'),
-    description: z.nullable(z.string()),
-    tags: z.array(z.string()).nullish(),
-    assignees: z.array(z.string()).nullish(),
-    startOn: z.date().optional().nullish(),
-    dueOn: z.date().optional().nullish(),
-    startedAt: z.date().optional().nullish(),
-    completedAt: z.date().optional().nullish(),
-  });
 
 type FormDrawerProps = {
     taskId: string;
@@ -82,13 +94,7 @@ const UpdateTaskDrawer = ({
     onClose
   }: FormDrawerProps) => {
     const taskQuery = useTask({ taskId });
-    const projectOptions = useProjectOptions();
-    const tagOptions = useTagOptions();
-    const usersOptions = useUserOptions();
     const updateTaskMutation = useUpdateTask();
-  
-    const defaultTags = taskQuery.data?.tags.map(label => label.id.toString());
-    const defaultAssignees = taskQuery.data?.assignees.map(assignee => assignee.id.toString());
 
     return (
       <>
@@ -113,95 +119,10 @@ const UpdateTaskDrawer = ({
             </>
           )}
         >
+
             {
                 taskQuery.data ? (
-                    <Form<UpdateTaskDTO['data'], typeof schema>
-                        id="update-task"
-                        onSubmit={async (values) => {
-                            await updateTaskMutation.mutateAsync({ data: values, taskId });
-                        }}
-                        options={{
-                            defaultValues: {
-                                title: taskQuery.data?.title,
-                                description: taskQuery.data?.description,
-                                tags: defaultTags,
-                                assignees: defaultAssignees,
-                                projectId: taskQuery.data?.project?.id.toString() 
-                            },
-                        }}
-                        schema={schema}
-                        >
-                        {({ register, formState, control }) => {
-                            return (
-                            <>
-                            <SelectField
-                                label='Project'
-                                options={projectOptions}
-                                defaultValue={taskQuery.data?.project?.id.toString()}
-                                error={formState.errors['projectId']}
-                                registration={register('projectId')}
-                                control={control}
-                            />
-                            <InputField
-                                label="Title"
-                                error={formState.errors['title']}
-                                registration={register('title')}
-                            />
-                            <TextAreaField
-                                label="Description"
-                                error={formState.errors['description']}
-                                registration={register('description')}
-                            />
-                            <SelectField
-                                label='Tags'
-                                options={tagOptions}
-                                defaultValue={defaultTags}
-                                error={formState.errors['tags']}
-                                registration={register('tags')}
-                                control={control}
-                                multiple={true}
-                            />
-                            
-                            <SelectField
-                                label='Assignees'
-                                options={usersOptions}
-                                defaultValue={defaultAssignees}
-                                error={formState.errors['assignees']}
-                                registration={register('assignees')}
-                                control={control}
-                                multiple={true}
-                            />
-                            <DatePicker 
-                                label='Start On'  
-                                mode='single' 
-                                name="startOn" 
-                                control={control} error={formState.errors['startOn']}
-                                defaultValue={taskQuery.data.startOn}
-                            />
-                            <DatePicker 
-                                label='Due On' 
-                                mode='single' 
-                                name="dueOn" 
-                                control={control} error={formState.errors['dueOn']}
-                                defaultValue={taskQuery.data.dueOn}
-                            />
-                            <DatePicker 
-                                label='Started At'  
-                                mode='single' 
-                                name="startedAt" 
-                                control={control} error={formState.errors['startedAt']}
-                                defaultValue={taskQuery.data.startedAt}
-                            />
-                            <DatePicker 
-                                label='Completed At' 
-                                mode='single' 
-                                name="completedAt" 
-                                control={control} error={formState.errors['completedAt']}
-                                defaultValue={taskQuery.data.completedAt}
-                            />
-                            </>
-                        )}}
-                    </Form>
+                    <FormUpdateTask onSubmit={async (values) => await updateTaskMutation.mutateAsync({ data: values, taskId })} taskId={taskQuery.data.id}/>
                 ) : (
                     <div className="w-full h-48 flex justify-center items-center">
                         <Spinner size="lg" />
